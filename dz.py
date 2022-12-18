69b54178c2148b1023ace5335d8cca5d27476a2c
Nadya <nad.anikina@gmail.com>
2022-12-18 00:36:38 +0000

    2 task

--- dz.py
+++ dz.py
@@ -1,51 +1,23 @@
-# Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму
+#my_number = (input('Введите число '))
+#sum=0
+#array_numbers =[]
+#for symbol in my_number:
+#    if '1234567890'.find(symbol) != -1:
+#        array_numbers.append(int(symbol))
+#for i in (array_numbers):
+#    sum=sum+i
+#print (sum)
 
-#Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0
-# и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится)
-
-# x_coordinate = int(input('Введите координату Х, неравную 0 '))
-# y_coordinate = int(input('Введите координату Y, неравную 0 '))
-# if x_coordinate>0 and y_coordinate>0:
-#     print('Это первая четверть')
-# elif x_coordinate<0 and y_coordinate>0:
-#     print('Это вторая четверть')
-# elif x_coordinate>0 and y_coordinate<0:
-#     print('Это четвертая четверть')
-# elif x_coordinate<0 and y_coordinate<0:
-#     print('Это третья четверть')
-# else:
-#     print('координаты не равны нулю по условию')
-#Напишите программу, которая по заданному номеру четверти, 
-# показывает диапазон возможных координат точек в этой четверти (x и y).
-
-# nomer_chetverti = int(input('Введите номер четверти от 1 до 4 '))
-# if nomer_chetverti == 1:
-#     print('Х и Y координаты положительные числа')
-# elif nomer_chetverti == 2:
-#     print('Х координата отрицательное, Y координата положительное числа')
-# elif nomer_chetverti == 3:
-#     print('Х и Y координаты отрицательные числа')
-# elif nomer_chetverti == 4:
-#     print('Х координата положительное, Y координата отрицательное число')
-# else:
-#     print('Вы ввели некорректные данные')
-
-#Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
-# import math
-# x1_coordinate = int(input('Введите координату Х первой точки '))
-# y1_coordinate = int(input('Введите координату Y первой точки '))
-# x2_coordinate = int(input('Введите координату Х второй точки '))
-# y2_coordinate = int(input('Введите координату Y второй точки '))
-# distance = math.sqrt(((x2_coordinate-x1_coordinate)**2)+((y2_coordinate-y1_coordinate)**2))
-# print (round(distance,3))
-
-#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр
-my_number = (input('Введите число '))
+#Задайте список из n чисел последовательности (1 + 1/n)**n, выведеите его на экран, а так же сумму элементов списка.
+number=int(input('Введите число: '))
+array_numbers=[]
+for i in range(1,number+1):
+     array_numbers.append((1 + 1/i)**i)
 sum=0
-array_numbers =[]
-for symbol in my_number:
-    if '1234567890'.find(symbol) != -1:
-        array_numbers.append(int(symbol))
-for i in (array_numbers):
-    sum=sum+i
-print (sum)
+for i in array_numbers:
+    sum+=i
+print(f'Для N ={number}:->', end='')
+print(array_numbers)
+print(f'Сумма', end=' ')
+print(round(sum,2))
