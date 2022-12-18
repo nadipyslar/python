0262ca9f44edf7c7745dbd9435f097ce4cd9774b
Nadya <nad.anikina@gmail.com>
2022-12-18 00:06:23 +0000

    1task

--- dz.py
+++ dz.py
@@ -31,10 +31,21 @@
 #     print('Вы ввели некорректные данные')
 
 #Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве
-import math
-x1_coordinate = int(input('Введите координату Х первой точки '))
-y1_coordinate = int(input('Введите координату Y первой точки '))
-x2_coordinate = int(input('Введите координату Х второй точки '))
-y2_coordinate = int(input('Введите координату Y второй точки '))
-distance = math.sqrt(((x2_coordinate-x1_coordinate)**2)+((y2_coordinate-y1_coordinate)**2))
-print (round(distance,3))
+# import math
+# x1_coordinate = int(input('Введите координату Х первой точки '))
+# y1_coordinate = int(input('Введите координату Y первой точки '))
+# x2_coordinate = int(input('Введите координату Х второй точки '))
+# y2_coordinate = int(input('Введите координату Y второй точки '))
+# distance = math.sqrt(((x2_coordinate-x1_coordinate)**2)+((y2_coordinate-y1_coordinate)**2))
+# print (round(distance,3))
+
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр
+my_number = (input('Введите число '))
+sum=0
+array_numbers =[]
+for symbol in my_number:
+    if '1234567890'.find(symbol) != -1:
+        array_numbers.append(int(symbol))
+for i in (array_numbers):
+    sum=sum+i
+print (sum)
