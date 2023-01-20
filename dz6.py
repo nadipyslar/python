5b65c665d2babe177b403c9da4c363a7980f922b
Nadya <nad.anikina@gmail.com>
2023-01-20 16:32:14 +0400

    2 tasks

---
+++ dz6.py
@@ -0,0 +1,17 @@
+#С помощью использования **лямбд, filter, map, zip, enumerate, list comprehension
+
+#Задайте список из нескольких чисел. 
+# Напишите программу, которая найдёт сумму элементов списка, стоящих на позиции с нечетным индексом
+
+#import random
+#my_list = [random.randint(0,100) for i in range (int(input('Введите количество элементов списка: ')))]
+#print(my_list)
+#odd_sum =sum(my_list[1::2])
+#print (odd_sum)
+
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму цифр
+#import functools
+#str = input('Введите число: ')
+#data = list(map(int, str.replace(',','')))
+#sum = functools.reduce((lambda a, b: a+b), data)
+#print (sum)
